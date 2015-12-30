c3b91022ddf3ecf4382a6af0b2cddd9c273876b5
Hemerson Vianna <hemerson.lourenco@gmail.com>
2015-12-30 06:28:27 -0200

    added user class

---
+++ classes/User.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'Crud.php';
+
+class User extends Crud {
+	
+	protected $table = 'users';
+	private $name;
+	private $email;
+
+	public function setNome($name){
+		$this->name = $name;
+	}
+
+	public function getNome(){
+		return $this->name;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function insert(){
+
+		$sql  = "INSERT INTO $this->table (name, email) VALUES (:name, :email)";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':email', $this->email);
+		return $stmt->execute(); 
+
+	}
+
+	public function update($id){
+
+		$sql  = "UPDATE $this->table SET name = :name, email = :email WHERE id = :id";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':email', $this->email);
+		$stmt->bindParam(':id', $id);
+		return $stmt->execute();
+
+	}
+
+}
